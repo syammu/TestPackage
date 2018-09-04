d16a5ea68a469864f895b5ec79534d92cc80a2e1
EEH <eli.holmes@noaa.gov>
2018-09-04 15:02:14 -0700

    Initial commit

---
+++ R/SSTplot.R
@@ -0,0 +1,33 @@
+#' SST off the West Coast of India
+#'
+#' This little function plots the SST off the western coast of India from remote sensing data.
+#'
+#' These data are from the \href{https://podaac.jpl.nasa.gov/dataset/UKMO-L4HRfnd-GLOB-OSTIA}{Global SST & Sea Ice Analysis},
+#' L4 OSTIA, UK Met Office,
+#' Global, 0.05°, Daily,2013-present dataset available on ERDDAP at
+#' \href{https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplUKMO_OSTIAv20}{ERDDAP}.
+#'
+#' The default is to show the
+#' latest SST values but you can enter another date.  The earliest year in this data
+#' set is 2013.
+#'
+#' @param date A character string in "2014-01-30" format. The default is "last" which is the latest values.
+#' @return A plot of SST.
+#' @examples
+#' SSTplot("2017-09-09")
+#' @export
+SSTplot <- function(date="last"){
+  if(date!="last"){
+    if(as.numeric(format(as.Date("2008-09-10"),"%Y"))<2013) stop("This SST data set only goes back to 2013")
+    date <- paste0(date,"T12:00:00Z")
+  }
+  url <- paste0(
+    "https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplUKMO_OSTIAv20.png?analysed_sst%5B(",
+    date,
+    ")%5D%5B(7.025):(20.025)%5D%5B(70.025):(78.025)%5D&.draw=surface&.vars=longitude%7Clatitude%7Canalysed_sst&.colorBar=%7C%7C%7C16%7C34%7C&.bgColor=0xffccccff&.trim=0&.size=300"
+    )
+  im <- load.image(url)
+  opar <- par(mar=c(0,0,0,0))
+  plot(im, axes=FALSE)
+  par(opar)
+}
---
+++ R/myarimaforecast.R
@@ -0,0 +1,19 @@
+#' Forecast with Arima Model
+#'
+#' This fits an Arima model to data with forecast's auto.arima() function and plots
+#' a forecast with the forecast() function.
+#'
+#' @param data A vector (time series) of data
+#' @param nyears Number of time steps to forecast forward
+#' @return A plot of a forecast.
+#' @examples
+#' dat <- WWWusage
+#' myarimaforecast(dat, nyears=100)
+#' @export
+myarimaforecast <- function(data, nyears=10){
+  # This is how I would write these 3 lines using a pipe
+  # data %>% auto.arima %>% forecast(h = nyears) %>% plot
+  fit <- auto.arima(data)
+  fc <- forecast(fit, h = nyears)
+  plot(fc)
+}
